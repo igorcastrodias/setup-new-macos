87176aa114e4148cec3eff380126a6d000287c0c
Igor Castro <igorcastro.dias@gmail.com>
2022-09-22 11:19:02 -0300

    Added homebrew in powershell path

--- 02-homebrew-packages.sh
+++ 02-homebrew-packages.sh
@@ -18,6 +18,9 @@ brew tap azure/functions
 brew install azure-functions-core-tools@4
 
 brew install --cask powershell
+echo "Add homebrew path in powershell"
+#Add-Content -Path $PROFILE.CurrentUserAllHosts -Value '$(/opt/homebrew/bin/brew shellenv) | Invoke-Expression'
+
 brew install --cask azure-data-studio
 brew install --cask microsoft-azure-storage-explorer
 brew install --cask postman
